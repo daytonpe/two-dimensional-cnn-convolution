dd9c3f74b13662aa15f5cbf6ee5c3e8f80417368
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2018-09-04 23:45:38 -0500

    zero padding done

--- hw1.py
+++ hw1.py
@@ -11,7 +11,7 @@ from random import randint
 
 
 input_feature_type = "sequential"  # {random, sequential}
-input_feature_map_size = (2, 5, 5)  # Ni, Lr, Lc (channels, rows cols)
+input_feature_map_size = (2, 5, 5)  # Ni, Lr, Lc (channels, rows, cols)
 input_feature_map_zero_padding = [1, 1, 1, 1]  # Pl, Pr, Pt, Pb
 input_feature_map_up_sampling_factor = [1, 1]  # Ur, Uc
 filter_coefficient_type = "sequential"  # {random, sequential}
@@ -53,10 +53,40 @@ def generate_data(feature_type, feature_map_size):
         return [[[]]]
 
 
+def pre_process(input_tensor, input_feature_map_zero_padding):
+
+    # zero padding
+
+    # for ease and code clarity
+    Pl = input_feature_map_zero_padding[0]
+    Pr = input_feature_map_zero_padding[1]
+    Pt = input_feature_map_zero_padding[2]
+    Pb = input_feature_map_zero_padding[3]
+
+    original_size = list(np.shape(input_tensor))
+    processed_size = list(np.shape(input_tensor))
+    processed_size[1] += (Pt + Pb)
+    processed_size[2] += (Pl + Pr)
+    processed_size_tuple = tuple(processed_size)
+
+    print('original_size', original_size)
+
+    padded_tensor = np.zeros(processed_size_tuple)
+
+    for i in range(original_size[0]):  # each channel
+        for j in range(Pt, Pt + original_size[1]):  # each row (not padding)
+            for k in range(Pl, Pl + original_size[2]):  # each col (not padding)
+                padded_tensor[i][j][k] = input_tensor[i][j-Pt][k-Pl]
+    return padded_tensor;
+
+
 ###############################################################################################
 input_tensor = generate_data(input_feature_type, input_feature_map_size)
 print('\nInput feature map:\n')
 print(input_tensor)
+
+print('\nInput feature map with zero padding:\n')
+print(pre_process(input_tensor, input_feature_map_zero_padding))
 
 filter_tensor = generate_data(filter_coefficient_type, filter_coefficient_size)
 print('\nFilter coefficients:\n')
