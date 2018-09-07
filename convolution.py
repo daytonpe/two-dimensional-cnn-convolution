9837879f69d3785a3975a580817003755071a3a4
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2018-09-06 19:08:30 -0500

    creates blank output matrix

--- convolution.py
+++ convolution.py
@@ -17,9 +17,13 @@ input_feature_map_up_sampling_factor = [1, 1]  # Ur, Uc
 filter_coefficient_type = "sequential"  # {random, sequential}
 filter_coefficient_size = (3, 2, 3, 3)  # No, Ni, Fr, Fc
 filter_coefficient_up_sampling_factor = [1, 1]  # Dr, Dc
-output_feature_map_channels = 1  # No
+output_feature_map_channels = 3  # No
 output_feature_map_down_sampling_factor = [1, 1]  # Sr, Sc
 
+No = output_feature_map_channels
+Ni = input_feature_map_size[0]
+Fr = filter_coefficient_size[2]
+Fc = filter_coefficient_size[3]
 
 def generate_data(feature_type, feature_map_size):
 
@@ -114,12 +118,17 @@ def combine_matrices(A, B):
 
 
 def createInputMatrix(X, F_size, Y_size):
+    Lr = np.shape(X)[1]
+    Lc = np.shape(X)[2]
+    Fr = F_size[2]
+    Fc = F_size[3]
+    Mr = Lr - Fr + 1
+    Mc = Lc - Fc + 1
+
     # turn input feature map into a matrix
     Ni = F_size[1]
     Fr = F_size[2]
     Fc = F_size[3]
-    Mr = Y_size[1]
-    Mc = Y_size[2]
 
     X_matrix = np.zeros((Ni * Fr * Fc, Mr * Mc))
     r = 0
@@ -136,13 +145,30 @@ def createInputMatrix(X, F_size, Y_size):
     print(X_matrix)
 
 
+def createBlankOutputMatrix(output_feature_map_channels, input_feature_map_size):
+    Lr = input_feature_map_size[1]
+    Lc = input_feature_map_size[2]
+    Fr = filter_coefficient_size[2]
+    Fc = filter_coefficient_size[3]
+    No = output_feature_map_channels
+    Mr = Lr - Fr + 1
+    Mc = Lc - Fc + 1
+    Y_matrix = np.zeros((No, Mr * Mc))
+    return Y_matrix
+
+
 ##############################################################################
 input_tensor = generate_data(input_feature_type, input_feature_map_size)
 print('\nInput feature map:\n')
 print(input_tensor)
 
+print('\nOutput matrix (blank)')
+Y_matrix = createBlankOutputMatrix(output_feature_map_channels, input_feature_map_size)
+Y_shape = np.shape(Y_matrix)
+print(Y_matrix)
+
 print('\nInput feature matrix size (no values yet):\n')
-createInputMatrix(input_tensor, filter_coefficient_size, (3, 3, 3))
+createInputMatrix(input_tensor, filter_coefficient_size, Y_shape)
 
 print('\nInput feature map with up sampling:\n')
 print(up_sampling(input_tensor, input_feature_map_up_sampling_factor))
@@ -161,6 +187,9 @@ output_tensor = np.zeros((filter_coefficient_size[0],
 
 print('\nOutput feature map:\n')
 print(output_tensor)
+
+
+
 
 # print('\n\n combine test\n\n')
 # print(combine_matrices(np.ones((3, 3)), np.zeros((3, 3,))))
